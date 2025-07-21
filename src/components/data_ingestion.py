f4ccf5a900be4c2000cd71e7224cffa02950341c
Abrar tintoiya <mohammadabrartintoiya@gmail.com>
2025-07-21 19:03:36 +0530

    data transform and Hyperperameter training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,11 +7,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
-    raw_data_path_input: str = "notebook/data/stud.csv"
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
@@ -48,8 +51,12 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 
-if __name__ == "__main__":
+if __name__=="__main__":
     obj=DataIngestion()
-    train_data_path, test_data_path = obj.initiate_data_ingestion()
-    print(f"Train data saved at: {train_data_path}")
-    print(f"Test data saved at: {test_data_path}")
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
