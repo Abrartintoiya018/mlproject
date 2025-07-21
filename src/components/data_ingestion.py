4119b3a1f5f3735197b105310cd2dddbfbb50a4d
Abrar tintoiya <mohammadabrartintoiya@gmail.com>
2025-07-21 14:40:44 +0530

    Data Tranformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+
+
 @dataclass
 class DataIngestionConfig:
+    raw_data_path_input: str = "notebook/data/stud.csv"
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
